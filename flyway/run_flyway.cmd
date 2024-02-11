b6b7f1ae3dfe0eabc368257fe256ddf35a8f87db
dale highfill <dhighfi2@jccc.edu>
2024-02-11 06:39:40 -0600

    convert to toml

--- flyway/run_flyway.cmd
+++ flyway/run_flyway.cmd
@@ -3,4 +3,4 @@ ECHO OFF
 set FLYWAY_HOME=U:\Users\Dale\Development\Flyway\flyway-9.4.0
 
 @REM This command will allow paramters to be passed such as info or migrate
-%FLYWAY_HOME%/flyway.cmd -configFiles="./conf/flyway.conf" %1 %2 %3 %4
+%FLYWAY_HOME%/flyway.cmd -configFiles="./conf/flyway.toml" %1 %2 %3 %4
