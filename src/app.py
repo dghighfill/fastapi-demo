5fe4088e20e74393e339ea57f48a9639eaf79c8d
dale highfill <dhighfi2@jccc.edu>
2024-02-11 10:59:16 -0600

    fix relative path

--- src/app.py
+++ src/app.py
@@ -17,7 +17,8 @@ from typing import Dict
 
 app = FastAPI()
 origins = [
-    "http://localhost:8080"
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
 ]
 app.add_middleware(CORSMiddleware,
     allow_origins=origins,
--- start_app.sh
+++ start_app.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
 cd flyway
 ./run_flyway.cmd clean migrate info
 cd -
+
 # Start the HTTP Server
-start python -m http.server -d ./ui/ 8080
-# Start API Server
-start python src/app.py
+start pipenv run python -m http.server -d ./ui/ 8080
+
+### Start API Server
+export SQLALCHEMY_DATABASE_URL="sqlite:///./coffee.db"
+start pipenv run python src/app.py
 
