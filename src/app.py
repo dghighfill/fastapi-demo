af7878a21bf50d510b4f388610a26676f463745a
dale highfill <dhighfi2@jccc.edu>
2024-09-22 17:08:32 -0500

    add back in cors

--- src/app.py
+++ src/app.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from coffee_router import router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+]
+app.add_middleware(CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 app.include_router(router)
 
 if __name__ == "__main__":
